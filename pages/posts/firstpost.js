b5aa707ec1a67fb6cfc814fbd23f510ea06ebb8f
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-06-25 23:57:10 +0600

    Image and Dynamic Title Added

--- pages/posts/firstpost.js
+++ pages/posts/firstpost.js
@@ -1,10 +1,34 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
+import Head from "next/head";
 
 const FirstPost = () => {
     return (
       <div>
+        <Head>
+          <title>Image</title>
+        </Head>
         <h1> First Post</h1>
+        <Image
+          src="/images/40948-Samiul.jpg"
+          height={80} // Desired size with correct aspect ratio
+          width={80} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
+        <Image
+          src="/images/40948-Samiul.jpg"
+          height={80} // Desired size with correct aspect ratio
+          width={80} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
+        <Image
+          src="/images/40948-Samiul.jpg"
+          height={80} // Desired size with correct aspect ratio
+          width={80} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
+
         <Link href="/">
           <a> Back To Home</a>
         </Link>
